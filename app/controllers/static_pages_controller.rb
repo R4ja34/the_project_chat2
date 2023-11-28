cf3f0d0c19500901b9af5a01be66efac037ee312
ho757uff <robindebessel.pro@gmail.com>
2023-11-28 13:50:24 +0100

    Created website backbone : added _navbar + _footer + staticpages + mod : routes + html.erb

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,9 @@
+class StaticPagesController < ApplicationController
+  def home
+    # Votre code pour l'action home ici
+  end
+
+  def login
+    # logique pour la page de connexion
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
- 
-  resources :items, only: [:index, :show]
+  # Defines the root path route ("/")
+  root "static_pages#home"
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "up" => "rails/health#show", :as => :rails_health_check
 
-  # Defines the root path route ("/")
   # root "posts#index"
+
+  # Static pages
+  get "about", to: "static_pages#about"
+  get "contact", to: "static_pages#contact"
 end
