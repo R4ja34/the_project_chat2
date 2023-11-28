91c48205d5fcc09fd7a9524ef13a7c6b734e63ef
R4ja34 <raja.lalaina@gmail.com>
2023-11-28 19:19:26 +0100

    set cart and add item

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,10 +1,32 @@
 class CartsController < ApplicationController
-  def show
-  end
+  before_action :authenticate_user!
+  before_action :create_cart_if_not_exists
 
-  def add_item
+  def show
+    @cart = current_user.cart
+    if @cart
+      @cart_items = @cart.cart_items.includes(:item)
+    else
+      flash[:alert] = "Vous n'avez pas de panier. Veuillez ajouter des articles."
+      redirect_to root_path
+    end
   end
 
   def remove_item
+    @item = Item.find(params[:item_id])
+    @cart = current_user.cart
+    if @cart
+      @cart.cart_items.where(item: @item).destroy_all
+      redirect_to cart_path, notice: 'Article retiré du panier!'
+    else
+      flash[:alert] = "Vous n'avez pas de panier. Veuillez créer un panier d'abord."
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def create_cart_if_not_exists
+    @cart = current_user.cart || Cart.create(user: current_user)
   end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,22 @@ class ItemsController < ApplicationController
     set_item()
   end
 
+  def add_to_cart
+    @item = Item.find(params[:id])
+    @cart = current_user.cart
 
+    if @cart
+      if @cart.cart_items.exists?(item_id: @item.id)
+        redirect_to cart_path, notice: 'Cet article est déjà dans votre panier!'
+      else
+        @cart_item = @cart.cart_items.create(item: @item)
+        redirect_to cart_path, notice: 'Article ajouté au panier!'
+      end
+    else
+      flash[:alert] = "Vous n'avez pas de panier. Veuillez créer un panier d'abord."
+      redirect_to root_path
+    end
+  end
 
   private
 
